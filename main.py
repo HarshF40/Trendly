d26938e3feba8a767f76583f611709055330a914
HarshF40 <harshgaonkar57@gmail.com>
2024-11-19 23:47:06 +0530

    add timer

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import streamlit as st
 import google.generativeai as genai
 from googleapiclient.discovery import build
 import re
+import time
 
 ##configuring genai and yt
 genai.configure(api_key=os.getenv("GEMINI_API"))
@@ -14,6 +15,15 @@ youtube = build('youtube','v3',developerKey=os.getenv("YOUTUBE_API")) ##youtube 
 input_prompt = """
 You are an expert song trend analyser. you predict on if the song will go in trend or viral in the shortform content  like reels,tiktok,shorts,etc. you predict by analysing the data of the video. like likes,dislikes,like dislike ratio with reference to the views. number of comments.types of comments like funny,making fun of the song,supportive,loving,bad,crticising by reading the top comments. and then giving your final answer in terms of percentage and a small justification
 """
+
+def show_timer(duration):
+    timer_placeholder = st.empty()  # Create a placeholder to update the timer
+    start_time = time.time()
+    
+    while time.time() - start_time < duration:
+        elapsed_time = time.time() - start_time
+        timer_placeholder.text(f'Time remaining: {300.00 - elapsed_time:.2f} seconds')
+        time.sleep(0.1)  # Update the timer every 0.1 seconds
 
 
 def get_video_data(video_id):
@@ -51,14 +61,26 @@ if st.button("Analyse") :
     if re.match(pattern,YTlink):
         video_id = YTlink.split("=")[1].split("&")[0]
         st.image(f"https://img.youtube.com/vi/{video_id}/0.jpg",use_container_width=True)
-        video_data = get_video_data(video_id)
-        if video_data :
-            st.subheader("Video Details")
-            st.write(f"**Title:** {video_data['title']}")
-            st.write(f"**Published At:** {video_data['published_at']}")
-            st.write(f"**Views:** {video_data['view_count']}")
-            st.write(f"**Likes:** {video_data['like_count']}")
-            st.write(f"**Dislikes:** {video_data['dislike_count']}")
-            st.write(f"**Comments:** {video_data['comment_count']}")
+        old_video_data = get_video_data(video_id)
+        st.success("Data fetched!")
+        show_timer(300)
+        latest_video_data = get_video_data(video_id)
+        st.success("Data fetched successfully!")
+        if old_video_data and latest_video_data :
+            st.subheader("Video Details(old)")
+            st.write(f"**Title:** {old_video_data['title']}")
+            ##st.write(f"**Published At:** {old_video_data['published_at']}")
+            st.write(f"**Views:** {old_video_data['view_count']}")
+            st.write(f"**Likes:** {old_video_data['like_count']}")
+            ##st.write(f"**Dislikes:** {old_video_data['dislike_count']}")
+            st.write(f"**Comments:** {old_video_data['comment_count']}")
+
+            st.subheader("\nVideo Details(new)")
+            st.write(f"**Title:** {latest_video_data['title']}")
+            ##st.write(f"**Published At:** {latest_video_data['published_at']}")
+            st.write(f"**Views:** {latest_video_data['view_count']}")
+            st.write(f"**Likes:** {latest_video_data['like_count']}")
+            ##st.write(f"**Dislikes:** {latest_video_data['dislike_count']}")
+            st.write(f"**Comments:** {latest_video_data['comment_count']}")
         else:
             st.error("Failed to fetch video data")
